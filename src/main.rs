4facdfd95b6785215f39b98bdb36a04420558f59
ahaoboy <504595380@qq.com>
2025-02-26 18:41:34 +0800

    cli: add rank

--- src/main.rs
+++ src/main.rs
@@ -17,10 +17,14 @@ fn main() {
 
     for i in guess_target(&name) {
         let name = format!("name: {}", i.name);
+        let rank = format!("rank: {}", i.rank);
         let target = format!("target: {}", i.target);
-        let version = i
-            .version
-            .map_or("".to_string(), |i| format!("version: {}", i));
-        println!("{name} {target} {version}");
+        let mut s = vec![name, rank, target];
+
+        if let Some(v) = i.version {
+            s.push(format!("version: {}", v));
+        }
+
+        println!("{}", s.join(" "));
     }
 }
